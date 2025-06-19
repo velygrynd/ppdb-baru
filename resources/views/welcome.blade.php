a444e2786571d533879b90228884b36fd4644a4b
velygrynd <prjcticon@gmail.com>
2025-06-19 22:27:01 +0700

    create-l

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold underline bg-blue-500 text-amber-300">
+      Hello world!
+    </h1>
+<div class="w-full h-screen flex justify-end p-8 bg-slate-950">
+        <div class="w-44 h-44 bg-rose-200 rounded-md m-6">
+            <div class="w-40 h-40 bg-blue-300 rounded-md"></div>
+        </div>
+    </div>
+  </body>
+</html>
